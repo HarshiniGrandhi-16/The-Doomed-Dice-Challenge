a4027870929a4f2706fbb65c46e53ecd22bae9ca
HarshiniGrandhi-16 <95412995+HarshiniGrandhi-16@users.noreply.github.com>
2024-03-25 16:24:06 +0530

    Create TotalCombinationPART(1).java

---
+++ TotalCombinationPART(1).java
@@ -0,0 +1,7 @@
+public class TotalCombinations {
+    public static void main(String[] args) {
+        int numFaces = 6;
+        int totalCombinations = numFaces * numFaces;
+        System.out.println("Total combinations possible: " + totalCombinations);
+    }
+}
