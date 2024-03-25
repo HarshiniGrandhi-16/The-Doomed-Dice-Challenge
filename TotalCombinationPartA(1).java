540e8d1b53f5669269631920c72be275cc87a94a
HarshiniGrandhi-16 <95412995+HarshiniGrandhi-16@users.noreply.github.com>
2024-03-25 16:54:02 +0530

    Delete TotalCombinationPartA(1).java

--- TotalCombinationPartA(1).java
+++
@@ -1,7 +0,0 @@
-public class TotalCombinations {
-    public static void main(String[] args) {
-        int numFaces = 6;
-        int totalCombinations = numFaces * numFaces;
-        System.out.println("Total combinations possible: " + totalCombinations);
-    }
-}
